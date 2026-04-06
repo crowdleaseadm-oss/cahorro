9a1d420bd5add995b1e8f14829c32cb0992cbd56
Crowd Lease <crowd.lease.adm@gmail.com>
2026-04-06 23:55:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-466254117-f8629",
+  "appId": "1:905579237132:web:fbf1a1fe4d455efc714329",
+  "apiKey": "AIzaSyAVivUgmWFHE1TAXmpBQpqFIVHh_KG3lOc",
+  "authDomain": "studio-466254117-f8629.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "905579237132"
+};
